1bb53906b0bd4e5a93cff68a1d88a853b7fcd9e5
Leanne <lkeeley@pace.edu>
2023-08-23 14:17:59 -0400

    add files and tests. preload code

---
+++ count_pronouns.py
@@ -0,0 +1,16 @@
+def main():
+    # an incomplete list of pronouns from https://www.thefreedictionary.com/List-of-pronouns.htm
+
+    pronouns_list = ['I', 'we', 'you', 'he', 'she', 'it', 'they', 'me', 
+                    'us', 'him', 'her', 'them', 'my', 'our', 'your', 'his', 
+                    'their', 'mine', 'yours', 'ours', 'hers', 'theirs', 
+                    'myself', 'ourselves', 'yourself', 'yourselves', 
+                    'himself', 'herself', 'itself', 'themselves']
+    
+    # write your code here
+
+    
+    return
+
+if __name__ == '__main__':
+    main()
