8d6a28d8abc84e2869d0d55e42e09ad95a25c0a8
Leanne <lkeeley@pace.edu>
2023-11-09 09:14:45 -0500

    update pronoun numbers

--- tests/count_pronouns_test.py
+++ tests/count_pronouns_test.py
@@ -2,13 +2,13 @@ import re
 import count_pronouns
 
 def test_count_pronouns_total(capsys):
-    output = '960'
+    output = '1186'
     count_pronouns.main()
     captured = capsys.readouterr()
     assert output in captured.out
 
 def test_count_pronouns_freq(capsys):
-    output = r'(?=.*you)(?=.*163)'
+    output = r'(?=.*you)(?=.*249)'
     count_pronouns.main()
     captured = capsys.readouterr()
     assert re.search(output, captured.out, re.IGNORECASE)
